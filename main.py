525d58e4a1e7812ffd51b5aec663e32d97da4a56
nazzal5448 <nazzal5448@gmail.com>
2025-06-20 23:16:40 +0500

    initial

--- main.py
+++ main.py
@@ -95,4 +95,5 @@ async def process_audio(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
